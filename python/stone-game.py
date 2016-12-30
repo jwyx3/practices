fc396fd3288e4c1298ba26efdae385ff39056505
jwyx3 <jwyx88003@gmail.com>
2016-12-30 19:59:27 +0000

    fix variable name

--- python/stone-game.py
+++ python/stone-game.py
@@ -5,9 +5,9 @@ class Solution:
     # @return {int} an integer
 
     # 区间类DP
-    # dp[i][k]: the min cost to merge stones start from i and the amount is k
-    # sums[k]: the sum of k stones start from 0
-    # dp[i][k] = sums[i+k] - sums[i] + min(dp[i][k-1]+dp[i+k-1][1],...,dp[i][1] + dp[i+1][k-1])
+    # dp[i][s]: the min cost to merge stones start from i and the amount is s
+    # sums[s]: the sum of s stones start from 0
+    # dp[i][s] = sums[i+s] - sums[i] + min(dp[i][s-1]+dp[i+s-1][1],...,dp[i][1] + dp[i+1][s-1])
     # initial: dp[i][1] = 0
     # ans: dp[0][n]
     def stoneGame(self, A):
@@ -15,14 +15,14 @@ class Solution:
             return 0
         n = len(A)
         sums = [0]
-        for k in xrange(1, n+1):
-            sums.append(sums[k-1] + A[k-1])
-        dp = [[0 if k == 1 else sys.maxint for k in xrange(n+1)] for i in xrange(n)]
+        for s in xrange(1, n+1):
+            sums.append(sums[s-1] + A[s-1])
+        dp = [[0 if s == 1 else sys.maxint for s in xrange(n+1)] for i in xrange(n)]
         # 大区间依赖小区间
-        for k in xrange(2, n+1):
-            for i in xrange(n-k+1):
+        for s in xrange(2, n+1):
+            for i in xrange(n-s+1):
                 # 递推式
-                for j in xrange(1, k):
-                    dp[i][k] = min(dp[i][k], sums[i+k] - sums[i] + dp[i][j] + dp[i+j][k-j])
+                for k in xrange(1, s):
+                    dp[i][s] = min(dp[i][s], sums[i+s] - sums[i] + dp[i][k] + dp[i+k][s-k])
         return dp[0][n]
 
