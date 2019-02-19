6487284db67c66a738d7bca9bd72b5cb5c4520ad
Yaoxuan Wang <jwyx88003@gmail.com>
2019-02-19 11:11:08 -0800

    more

--- leetcode/bfs/shortest-path-visiting-all-nodes.py
+++ leetcode/bfs/shortest-path-visiting-all-nodes.py
@@ -28,3 +28,60 @@ class Solution(object):
                     q.append((state2, nei))
         return -1
         
+# normal template with different way to check visited.
+
+class Solution(object):
+    def shortestPathLength(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: int
+        """
+        N = len(graph)
+        q = collections.deque([(1 << x, x) for x in xrange(N)])
+        visited = collections.defaultdict(int)
+        target = (1 << N) - 1
+        steps = 0
+        while q:
+            size = len(q)
+            for _ in xrange(size):
+                state, head = q.popleft()
+                if state == target:
+                    return steps
+                if visited[state, head]: continue
+                visited[state, head] = 1
+                for nei in graph[head]:
+                    q.append((state | (1 << nei), nei))
+            steps += 1
+        return -1
+
+# Less memory + less time. -> because less node in queue. 
+class Solution(object):
+    def shortestPathLength(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: int
+        """
+        N = len(graph)
+        q = collections.deque([(1 << x, x) for x in xrange(N)])
+        visited = [[0] * N for _ in xrange(1 << N)]
+        for x in xrange(N):
+            visited[1 << x][x] = 1
+        target = (1 << N) - 1
+        steps = 0
+        # how to understand the position of visited.
+        # it depends on the definition of queue.
+        # 1. check and mark visited after pop from queue. so queue can contain visited node.
+        # 2. check and mark visited after insert from queue. so queue can't contain visited node.
+        while q:
+            size = len(q)
+            for _ in xrange(size):
+                state, head = q.popleft()
+                if state == target:
+                    return steps
+                for nei in graph[head]:
+                    state2 = state | (1 << nei)
+                    if not visited[state2][nei]:
+                        visited[state2][nei] = 1
+                        q.append((state2, nei))
+            steps += 1
+        return -1
